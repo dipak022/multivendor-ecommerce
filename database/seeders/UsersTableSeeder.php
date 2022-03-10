54f4b10fb0050db4f4eb8c7da86b00c74071bd72
dipak022 <dipakd3ebnath4002@gmail.com>
2022-03-10 18:08:26 +0600

    create laravel project with seeder data added inital

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+class UsersTableSeeder extends Seeder
+{
+    
+    /**
+     * 
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \DB::table('users')->insert([
+            // Admin 
+            [
+                'full_name'=> 'Dipak Admin',
+                'username'=> 'Admin',
+                'email'=> 'admin@gmail.com',
+                'password'=>Hash::make('11111111'),
+                'role'=> 'admin',
+                'status'=> 'active',
+
+            ],
+            //vendor 
+            [
+                'full_name'=> 'Dipak vendor',
+                'username'=> 'vendor',
+                'email'=> 'vendor@gmail.com',
+                'password'=>Hash::make('11111111'),
+                'role'=> 'vendor',
+                'status'=> 'active',
+
+            ],
+            //customer
+            [
+                'full_name'=> 'Dipak customer',
+                'username'=> 'customer',
+                'email'=> 'customer@gmail.com',
+                'password'=>Hash::make('11111111'),
+                'role'=> 'customer',
+                'status'=> 'active',
+
+            ],
+
+
+            ]);
+    }
+}
