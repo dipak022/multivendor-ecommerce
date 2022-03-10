0ee9f820d2e021a799fa05232e383da327831efd
dipak022 <dipakd3ebnath4002@gmail.com>
2022-03-10 21:13:49 +0600

    Admin tamplate setup

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -37,4 +37,10 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    public function credentials(Request $request)
+    {
+        return ['email'=>$request->email,'password'=>$request->password,'status'=>'active','role'=>'admin'];
+ 
+    }
 }
---
+++ resources/views/backend/layouts/footer.blade.php
@@ -0,0 +1,11 @@
+
+<!-- Javascript -->
+<script src="{{asset('backend/')}}/asset/bundles/libscripts.bundle.js"></script>    
+<script src="{{asset('backend/')}}/asset/bundles/vendorscripts.bundle.js"></script>
+
+<script src="{{asset('backend/')}}/asset/bundles/chartist.bundle.js"></script>
+<script src="{{asset('backend/')}}/asset/bundles/knob.bundle.js"></script> <!-- Jquery Knob-->
+<script src="{{asset('backend/')}}/assets/vendor/toastr/toastr.js"></script>
+
+<script src="{{asset('backend/')}}/asset/bundles/mainscripts.bundle.js"></script>
+<script src="{{asset('backend/')}}/asset/js/index.js"></script>
