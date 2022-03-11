3e351f66b4c0636d84d77a761a038607f722ee6f
dipak022 <dipakd3ebnath4002@gmail.com>
2022-03-12 02:20:34 +0600

    Product databse create

---
+++ database/migrations/2022_03_11_200637_create_products_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->mediumText('summary')->nullable();
+            $table->longText('description')->nullable();
+            $table->integer('strock')->default(0);
+            $table->unsignedBigInteger('brand_id');
+            $table->unsignedBigInteger('cat_id');
+            $table->unsignedBigInteger('clild_cat_id')->nullable();
+            $table->string('photo');
+            $table->float('price')->default(0);
+            $table->float('offer_price')->default(0);
+            $table->float('discount')->default(0);
+            $table->string('size');
+            $table->enum('conditions',['new','popular','winter'])->default('new');
+            $table->unsignedBigInteger('vandor_id')->nullable();
+            $table->enum('status',['active','inactive'])->default('active');
+
+            $table->foreign('brand_id')->references('id')->on('brands')->onDelete('cascade');
+            $table->foreign('cat_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->foreign('clild_cat_id')->references('id')->on('categories')->onDelete('SET NULL');
+            $table->foreign('vandor_id')->references('id')->on('users')->onDelete('SET NULL');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
