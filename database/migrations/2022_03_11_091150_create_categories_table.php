c2cb25fce091e465eee4d7fea237f9f7312a018a
dipak022 <dipakd3ebnath4002@gmail.com>
2022-03-11 15:22:52 +0600

    Category database and controller create

---
+++ database/migrations/2022_03_11_091150_create_categories_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->string('photo')->nullable();
+            $table->boolean('is_parent')->default(true);
+            $table->Text('sammary')->nullable();
+            $table->unsignedBigInteger('parent_id');
+            $table->foreign('id')->references('categories')->onDelete('SET NULL');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+};
