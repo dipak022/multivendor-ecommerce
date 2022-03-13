3a741dba8c1d1f1684f198e42ee136d8dc9e8300
dipak022 <dipakd3ebnath4002@gmail.com>
2022-03-14 00:04:36 +0600

    error solve

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -183,7 +183,7 @@
                                     </div>
 
                                     <p class="brand_name">{{ \App\Models\Brand::where('id',$nproduct->brand_id)->value('title') }}</p>
-                                    <a href="{{ route('product.detail',$item->slug) }}">{{ucfirst($nproduct->title)}}</a>
+                                    <a href="{{ route('product.detail',$nproduct->slug) }}">{{ucfirst($nproduct->title)}}</a>
                                     <h6 class="product-price">{{number_format($nproduct->offer_price,2)}} TK <span><del class="text-danger">{{number_format($nproduct->price,2)}}</del></span></h6>
                                 </div>
                             </div>
