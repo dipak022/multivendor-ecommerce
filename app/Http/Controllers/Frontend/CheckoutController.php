cffdea08c748e878fe8697c18160bb4ebfca983a
dipak022 <dipakd3ebnath4002@gmail.com>
2022-03-16 19:25:29 +0600

    Gmail order mail send

--- app/Http/Controllers/Frontend/CheckoutController.php
+++ app/Http/Controllers/Frontend/CheckoutController.php
@@ -134,12 +134,12 @@ class CheckoutController extends Controller
         $order['scity'] =Session::get('checkout')['scity'];
         $order['sstreet'] =Session::get('checkout')['sstreet'];
         $order['spostcode'] =Session::get('checkout')['spostcode'];
-        Mail::to($order['email'])->bcc($order['semail'])->cc('dipakdebnath4022@mail.com')->send(new OredrMail($order));
-        dd('Mail is send');
+        
 
         $status = $order->save();
         if($status){
-            
+            Mail::to($order['email'])->bcc($order['semail'])->cc('dipakdebnath4022@mail.com')->send(new OredrMail($order));
+            //dd('Mail is send');
             Cart::instance('shopping')->destroy();
             Session::forget('coupon');
             Session::forget('checkout');
