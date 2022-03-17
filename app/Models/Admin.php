b35af4fc1c811aed83a92868bf2cc6dab88f97b8
dipak022 <dipakd3ebnath4002@gmail.com>
2022-03-18 02:26:02 +0600

    admin and seller auth gurd create

---
+++ app/Models/Admin.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\Admin as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+class Admin extends Authenticatable
+{
+    use HasFactory;
+    protected $guard='admins';
+    protected $fillable = [
+        'full_name',
+        'email',
+        'password',
+        'status',
+       
+    ];
+}
+}
--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -19,36 +19,47 @@ class UsersTableSeeder extends Seeder
         \DB::table('users')->insert([
             // Admin 
             [
-                'full_name'=> 'Dipak Admin',
-                'username'=> 'Admin',
-                'email'=> 'admin@gmail.com',
-                'password'=>Hash::make('11111111'),
-                'role'=> 'admin',
-                'status'=> 'active',
-
-            ],
-            //vendor 
-            [
-                'full_name'=> 'Dipak seller',
-                'username'=> 'seller',
-                'email'=> 'seller@gmail.com',
-                'password'=>Hash::make('11111111'),
-                'role'=> 'seller',
-                'status'=> 'active',
-
-            ],
-            //customer
-            [
-                'full_name'=> 'Dipak customer',
-                'username'=> 'customer',
-                'email'=> 'customer@gmail.com',
+                'full_name'=> 'Dipak User',
+                'username'=> 'User',
+                'email'=> 'user@gmail.com',
                 'password'=>Hash::make('11111111'),
                 'role'=> 'customer',
                 'status'=> 'active',
 
             ],
-
-
+        
             ]);
+
+        \DB::table('admins')->insert([
+            //vendor 
+            [
+                'full_name'=> 'Dipak admin',
+                'email'=> 'admin@gmail.com',
+                'password'=>Hash::make('11111111'),
+                //'role'=> 'admin',
+                'status'=> 'active',
+
+            ],
+        
+            ]);
+                
+                
+            \DB::table('sellers')->insert([
+                //customer
+                [
+                    'full_name'=> 'Dipak seller',
+                    'username'=> 'seller',
+                    'email'=> 'seller@gmail.com',
+                    'password'=>Hash::make('11111111'),
+                    //'role'=> 'seller',
+                    'status'=> 'active',
+
+                ],
+            
+            ]);    
+
+              
+             
+           
     }
 }
